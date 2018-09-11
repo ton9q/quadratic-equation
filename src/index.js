8b07ef7c04dc00fce05c7ec5c43d0ddad0fedaa9
ton9q <kuchma_anton@mail.ru>
2018-09-11 22:48:22 +0300

    addiction of logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  equation = equation.replace(/\s/g,''); // remove spaces
+
+  var elements = equation.split('*x');
+  
+  let a = elements[0];
+  let b = elements[1].slice(2);
+  let c = elements[2];
+
+  let d = Math.pow(b,2) - 4 * a * c;
+
+  if (d < 0) return ;
+
+  let x1 = Math.round((-Math.sqrt(d)) - b) / (2 * a);
+  let x2 = Math.round((Math.sqrt(d)) - b) / (2 * a);
+
+  return [x1,x2].sort((a,b) => a - b); 
+};
